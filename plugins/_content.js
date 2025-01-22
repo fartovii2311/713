7a7f0dc9a4aaa0b81560975eb30cfa1a85c6cf2e
fartovii2311 <fartovii2311@gmail.com>
2025-01-22 18:09:57 +0300

    уен

--- plugins/_content.js
+++ plugins/_content.js
@@ -22,9 +22,9 @@ global.canal4 = channels.channel4
 global.accountsgb = [canal1, canal2, canal3, canal4, tk, ig, yt, paypal, fb, ths, md, asistencia].getRandom()
 
 var canalesInfo = [
-{ link: canal1, id: "120363160031023229@newsletter", name: "Лилит 💫" },
+{ link: canal1, id: "120363160031023229@newsletter", name: "Галатея 💫" },
 { link: canal2, id: "120363169294281316@newsletter", name: "GB - UPDATE ✨" },
-{ link: canal3, id: "120363203805910750@newsletter", name: "Tips sobre Лилит 🤩" } 
+{ link: canal3, id: "120363203805910750@newsletter", name: "Tips sobre Галатея 🤩" } 
 ]
 var indiceAleatorio = Math.floor(Math.random() * canalesInfo.length)
 var channelRD = canalesInfo[indiceAleatorio]
